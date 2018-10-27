1b267f3e793392d5e982de614f9fc4826164c2f2
jvdevries <johan.vincent.de.vries@gmail.com>
2018-10-27 05:28:12 +0200

    java compilerHang test

---
+++ volatile/Java volatile APPEARS to fix Caching and Swapping (Java)/compilerHangVolatile.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class compilerHangVolatile {
+ private volatile boolean stop = false;
+
+ private void Run(int timeOut) {
+   var t2 = new Thread() {
+    public void run() {
+     try {
+      Thread.sleep(timeOut * 1000);
+      System.out.println("Timeout occured.");
+      System.exit(0);
+     } catch (InterruptedException e) {}
+     stop = true;
+    }
+   };
+   t2.start();
+   var t1 = new Thread() {
+    public void run() {
+     try {
+      Thread.sleep(100);
+     } catch (InterruptedException e) {}
+     stop = true;
+    }
+   };
+   t1.start();
+
+   while (!stop) ;
+   
+   System.out.println("Loop ended.");
+   System.exit(0);
+
+   try {
+    t2.join();
+   } catch (InterruptedException e) {}
+   try {
+    t1.join();
+   } catch (InterruptedException e) {}
+ }
+
+ public static void main(String[] args) {
+  var e = new compilerHangVolatile();
+  e.Run(Integer.parseInt(args[0]));
+ }
+}
